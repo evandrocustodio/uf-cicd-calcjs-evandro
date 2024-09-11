e46dc495a86e98ddc7cc8a926c044ab5d00753e0
EVANDRO C GONCALVES <evandrocustodio@gmail.com>
2024-09-11 03:02:34 +0000

    Adicionando funcao div

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,8 +3,17 @@
         return a - b;
     }
 
+    function divide(a, b) {
+        if (b === 0) {
+        throw new Error("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+
     // Exportar as funções
     module.exports = {
         add,
-        subtract
+        subtract,
+        divide
+
     };
