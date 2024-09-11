051dd3dff715dbdf36a06522f1913a45950ad580
EVANDRO C GONCALVES <evandrocustodio@gmail.com>
2024-09-11 03:00:30 +0000

    Adicionando funcao subtracao

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,10 @@
-// Função de adição
-    function add(a, b) {
-        return a + b;
-    }   
+// Função de subtração
+    function subtract(a, b) {
+        return a - b;
+    }
 
     // Exportar as funções
     module.exports = {
-        add
-    };    
+        add,
+        subtract
+    };
